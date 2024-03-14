1e671356114acbbdac5b33066a9dd3f43fab0775
허 준 <139440519+Junkai336@users.noreply.github.com>
2024-03-14 19:53:35 +0900

    [Bronze V] Title: 개, Time: 124 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("|\\_/|\n");
+        System.out.printf("|q p|   /}\n");
+        System.out.printf("( 0 )\"\"\"\\\n");
+        System.out.printf("|\"^\"`    |\n");
+        System.out.printf("||_/=\\\\__|");
+    }
+}
