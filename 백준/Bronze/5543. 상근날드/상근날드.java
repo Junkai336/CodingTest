fa2f4f27f80fa80c1e71c547948d9f24e5c9b3cb
허 준 <139440519+Junkai336@users.noreply.github.com>
2024-04-05 21:19:04 +0900

    [Bronze IV] Title: 상근날드, Time: 120 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/5543. 상근날드/상근날드.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+        int D = Integer.parseInt(br.readLine());
+        int E = Integer.parseInt(br.readLine());
+
+        int bug = Math.min(Math.min(A, B), Math.min(B, C));
+        int col = Math.min(D, E);
+
+        bw.write(String.valueOf(bug + col - 50));
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
