e701a599fb90b3587b2263839173acbfd0193c11
허 준 <139440519+Junkai336@users.noreply.github.com>
2024-05-19 17:30:41 +0900

    [Bronze I] Title: 약수, Time: 124 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] K = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            K[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(K);
+
+        int small = K[0];
+        int large = K[K.length -1];
+
+        System.out.println(small * large);
+    }
+
+}
