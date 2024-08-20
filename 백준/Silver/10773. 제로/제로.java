6c8a11116ccd23cf8a424273bfc734f42872c732
허 준 <139440519+Junkai336@users.noreply.github.com>
2024-08-20 16:50:09 +0900

    [Silver IV] Title: 제로, Time: 248 ms, Memory: 28692 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Stack<Integer> stack = new Stack<>();
+
+        int K = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < K; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int k = Integer.parseInt(st.nextToken());
+
+            if (k != 0) {
+                stack.push(k);
+            } else {
+                stack.pop();
+            }
+        }
+
+        int sum = 0;
+
+        // stack에서 요소 빼는데 stack의 사이즈를 계속 참조하면 길이가 안맞음. 비어있는지 확인하면서 반복 (iterator 처럼)
+        while (!stack.isEmpty()) {
+            sum += stack.pop();
+        }
+
+        System.out.println(sum);
+
+    }
+}
