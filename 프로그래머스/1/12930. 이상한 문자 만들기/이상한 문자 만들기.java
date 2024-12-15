3cdae666ac40fcad7d124cb76334de8f67e6abd6
허 준 <139440519+Junkai336@users.noreply.github.com>
2024-12-15 14:24:05 +0900

    Update 이상한 문자 만들기.java

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,22 +1,19 @@
 class Solution {
     public String solution(String s) {
-        StringBuilder newS = new StringBuilder(); // 결과 문자열
-        int idx = 0; // 단어 내 인덱스
+        String[] array = s.split(" ", -1);
+        String newS = "";
         
-        for (char c : s.toCharArray()) {
-            if (c == ' ') { // 공백 처리
-                newS.append(" ");
-                idx = 0; // 단어가 끝났으므로 인덱스 초기화
-            } else {
-                if (idx % 2 == 0) { // 짝수 인덱스
-                    newS.append(Character.toUpperCase(c));
-                } else { // 홀수 인덱스
-                    newS.append(Character.toLowerCase(c));
+        for(int i = 0; i < array.length; i++) {
+            if(i != 0) newS += " ";
+            for(int j = 0; j < array[i].length(); j++) {
+                if(j % 2 == 0) {
+                    newS += String.valueOf(Character.toUpperCase(array[i].charAt(j)));
+                } else {
+                    newS += String.valueOf(Character.toLowerCase(array[i].charAt(j)));
                 }
-                idx++; // 단어 내 인덱스 증가
             }
         }
         
-        return newS.toString();
+        return newS;
     }
 }
