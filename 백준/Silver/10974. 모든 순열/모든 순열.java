ce8b7c2ec8199bc307a31db46ceab332b4866297
허 준 <139440519+Junkai336@users.noreply.github.com>
2025-05-18 03:39:17 +0900

    모든 순열 : 방식 설명 정상화

--- 백준/Silver/10974. 모든 순열/모든 순열.java
+++ 백준/Silver/10974. 모든 순열/모든 순열.java
@@ -12,10 +12,27 @@ public class Main {
 
         방식
             순열 완성되면 stringbuilder에 넣기
-            1 -> 2 -> 3 = dfs(1)
-            1 -> 3 -> 2 = dfs(2)
-            2 -> 1 -> 3 = dfs(2)
-            2 -> 3 -> 1 = dfs(2)
+
+            0. start = 지금까지 몇개 골랐는가?
+            0. i = 어떤 숫자를 선택할 것인가?
+            
+            1. 맨 처음 for문 돌 때 [1] 넣고 dfs(1) 호출
+            2. dfs(1) 호출되면 visited[1]이 true니까 [1, 2]                     <start = 1> <i = 2>
+            3. dfs(2) 호출되면 visited[1]과 visited[2]가 true니까 [1, 2, 3]     <start = 2> <i = 3>
+            4. [1, 2, 3] 중 [3] 넣고 dfs(3) 호출
+            5. start == N 이니까 배열 출력하고 return
+            
+            6. dfs(3)을 부른 dfs(2)의 뒤에 코드가 실행되어야 하니까 visited[3]를 false로 바꾸고 코드 종료
+            7. dfs(2)에서 visited[2]를 false로 바꾸고 코드 종료
+            8. dfs(1)의 for문에서 visited[1]하고 visited[2]는 if문 조건을 충족하지 않았으니까 이미 넘겼고, i가 3인 상태임
+            9. 배열은 여전히 [1, 2, 3]인 상태
+            10. dfs(1)에서 visited[3]가 false니까 [3] 넣고 dfs(2) 호출 = start가 1이니까 result[start], 즉 result[1]에 3 넣음 [1, 3]
+            11. dfs(2)에서 visited[2]가 false니까 [1, 3, 2] 넣고 dfs(3) 호출
+
+            ...
+
+            12. dfs(1)은 for문 다 돌았으니까 dfs(0)으로 돌아가고 i가 2가 됨
+            13. 반복
 
         설명
             N! : 팩토리얼 (1 * 2 * 3 * ... * N) => 1부터 N까지로 만들 수 있는 순열 총 개수
